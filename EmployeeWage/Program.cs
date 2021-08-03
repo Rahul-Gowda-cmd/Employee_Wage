fa6d8a2ff07bf7a6c0b468e99e64ade3117f5327
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-03 23:49:41 +0530

    feat:Employee Monthly Wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -13,31 +13,38 @@ namespace EmployeeWage
             Console.WriteLine("Welcome to Employee Wage Program");
             int perHourWage = 1000;
             int HoursWorked = 8;
-            int dailyWage = 0;
+            int dailyWage;
             int PartTimeHours = 4;
+            int monthlyWage = 0;
             Random rm = new Random();
-            int workStatus = rm.Next(0, 3);
-
-            switch(workStatus)
+            for (int i = 0; i < 20; i++)
             {
-                case 0:
-                    Console.WriteLine("Employee is absent");
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is present");
-                    dailyWage = perHourWage * HoursWorked;
-           
-                    break;
-                case 2:
-                    Console.WriteLine("Employee has worked partTime");
-                    dailyWage = perHourWage * PartTimeHours;
-              
-                    break;
-                default:
-                    Console.WriteLine("Something Wrong Appeared");
-                    break;
+                dailyWage = 0;
+                int workStatus = rm.Next(0, 3);
+
+                switch (workStatus)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is absent");
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is present");
+                        dailyWage = perHourWage * HoursWorked;
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee has worked partTime");
+                        dailyWage = perHourWage * PartTimeHours;
+
+                        break;
+                    default:
+                        Console.WriteLine("Something Wrong Appeared");
+                        break;
+                }
+                Console.WriteLine("Daily Wage of Employee:" + dailyWage);
+                monthlyWage += dailyWage;
             }
-            Console.WriteLine("Daily Wage of Employee:" + dailyWage);
+            Console.WriteLine("MonthlyWage" + monthlyWage);
         }
     }
 }

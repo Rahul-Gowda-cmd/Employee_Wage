60b64bf583372a0396568f05228171fd7830858b
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-03 23:26:04 +0530

    feat:Daily Wage of Employee Calculated

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -11,17 +11,22 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
+            int perHourWage = 1000;
+            int HoursWorked = 8;
+            int dailyWage = 0;
             Random rm = new Random();
             int present = rm.Next(0, 2);
 
             if (present == 1)
             {
                 Console.WriteLine("Employee is present");
+                dailyWage = perHourWage * HoursWorked;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+            Console.WriteLine("Daily Wage of Employee:" + dailyWage);
         }
     }
 }
